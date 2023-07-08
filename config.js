e78881e3769fb1e8262ccea6de1f2624a2b4196e
Jānis Ieviņš <janis.ievins@testdevlab.com>
2023-07-08 14:12:15 +0300

    test

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const config = {
         host: 'https://gorest.co.in/public/v2',
         token: 'e3800b50df1afd29775ee8d0bc53d1435a4cb5924dcd3a99ed6588f298b55aeb',
         username: 'Tenali Ramakrishna',
-        gender: 'male',
+        gender: 'female',
         status: 'active'
     },
     PROD: {
